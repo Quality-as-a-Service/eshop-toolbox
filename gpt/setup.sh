0811cc099ea6b38e04212cbe18acb33d5aa99be4
cherniaev egor <chrnyaevek@gmail.com>
2023-06-05 15:59:09 +0200

    Test setup

--- gpt/setup.sh
+++ gpt/setup.sh
@@ -1,5 +1,11 @@
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
+
+if [ ! -f .env ]
+then
+  export $(cat .env | xargs)
+fi
+
 DJANGO_SUPERUSER_USERNAME=$DJANGO_SUPERUSER_USERNAME \
 DJANGO_SUPERUSER_PASSWORD=$DJANGO_SUPERUSER_PASSWORD \
 DJANGO_SUPERUSER_EMAIL="$DJANGO_SUPERUSER_EMAIL" \
