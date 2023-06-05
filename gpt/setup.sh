b55794f1e79f469ebf8593897a6aeb3ae8fa50bc
cherniaev egor <chrnyaevek@gmail.com>
2023-06-05 16:21:53 +0200

    fix setup file

--- gpt/setup.sh
+++ gpt/setup.sh
@@ -1,12 +1,9 @@
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
-if [ ! -f .env ]
-then
-  export $(cat .env | xargs)
-fi
 
-DJANGO_SUPERUSER_USERNAME=$DJANGO_SUPERUSER_USERNAME \
-DJANGO_SUPERUSER_PASSWORD=$DJANGO_SUPERUSER_PASSWORD \
-DJANGO_SUPERUSER_EMAIL="$DJANGO_SUPERUSER_EMAIL" \
+export DJANGO_SUPERUSER_USERNAME=admin
+export DJANGO_SUPERUSER_PASSWORD=admin
+export DJANGO_SUPERUSER_EMAIL=example@email.com
+
 python manage.py createsuperuser --noinput
