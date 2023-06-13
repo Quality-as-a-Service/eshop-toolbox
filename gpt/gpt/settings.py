a0bfcb0efe2766b87ffddd25c44d530c0801503d
cherniaev egor <chrnyaevek@gmail.com>
2023-06-13 19:10:02 +0200

    Disable debug

--- gpt/gpt/settings.py
+++ gpt/gpt/settings.py
@@ -28,9 +28,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-oc7&ai^1q0ps@(^ey3+_bjm0&70tfr5l2nixwxol)v3a)l-iu5'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost',
                  os.environ['DJANGO_PRODUCTION_IP'],
                  os.environ['DJANGO_PRODUCTION_DOMAIN']]
