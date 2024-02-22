11f69b5cc1ddc7a6cafec944ac93478b795d7e18
cherniaev egor <chrnyaevek@gmail.com>
2024-02-22 17:57:59 +0100

    Fix column names

--- gpt/gpt/core/handlers.py
+++ gpt/gpt/core/handlers.py
@@ -34,13 +34,24 @@ def handle_uploaded_file(request, payload, tag):
             log.append('File extension unknown.')
             return log
 
-        assert settings.EXCEL_PROMPT_COLUMN in df.columns
+        columns = [c.strip() for c in df.columns.values]
+        assert settings.EXCEL_PROMPT_COLUMN in columns
         try:
-            assert settings.EXCEL_KEY_COLUMN in df.columns
+            assert settings.EXCEL_KEY_COLUMN in columns
         except AssertionError:
             pass
         else:
             do_keys = True
+        
+        prompt_col = df.columns.values[columns.index(settings.EXCEL_PROMPT_COLUMN)]
+        product_sku_col = None
+        if do_keys:
+            product_sku_col = df.columns.values[columns.index(settings.EXCEL_KEY_COLUMN)]
+
+    if do_keys:
+        log.append('Product SKU column is detected.')
+    else:
+        log.append('Product SKU column is not detected.')
 
     log.append('File format ok.')
 
@@ -55,15 +66,8 @@ def handle_uploaded_file(request, payload, tag):
 
     prompts = []
     for i, row in df.iterrows():
-        prompt_text = str(row[settings.EXCEL_PROMPT_COLUMN])
-        prompt_key = str(row[settings.EXCEL_KEY_COLUMN]) if do_keys else None
-
-        try:
-            prompt_text = clean_html(prompt_text)
-        except Exception as e:
-            logger.warn(e)
-            log.append(f'Skip: row: {i}, text: "{prompt_text}" ')
-            continue
+        prompt_text = str(row[prompt_col])
+        prompt_key = str(row[product_sku_col]) if do_keys else None
 
         if prompt_key is not None:
             try:
@@ -72,6 +76,13 @@ def handle_uploaded_file(request, payload, tag):
                 logger.warn(e)
                 log.append(f'Skip: row: {i}, key: "{prompt_key}" ')
                 continue
+
+        try:
+            prompt_text = clean_html(prompt_text)
+        except Exception as e:
+            logger.warn(e)
+            log.append(f'Skip: row: {i}, text: "{prompt_text}" ')
+            continue
 
         prompt = models.Prompt(
             dataset=dataset,
