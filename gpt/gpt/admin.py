a65cece824c4512d758ead525d41e0d211c5fe60
cherniaev egor <chrnyaevek@gmail.com>
2024-10-09 19:15:44 +0200

    Refine

--- gpt/gpt/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from gpt import models
-
-admin.site.register(models.GPTModel)
-admin.site.register(models.Api)
-admin.site.register(models.Dataset)
-admin.site.register(models.Prompt)
-admin.site.register(models.Completition)
-admin.site.register(models.EvaluationIteration)
--- gpt/gpt/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class UploadFileForm(forms.Form):
-    tag = forms.CharField(max_length=50, help_text='Unique data name.')
-    file = forms.FileField()
