b7bda87648b840f74af1d2bcf9aed4bbc99cb05d
cherniaev egor <chrnyaevek@gmail.com>
2024-10-15 17:43:25 +0200

    Decrease update frequency

--- watchdogs/function_app.py
+++ watchdogs/function_app.py
@@ -5,7 +5,7 @@ from manager import Manager
 app = func.FunctionApp()
 
 @app.function_name(name="realitymarket")
-@app.timer_trigger(schedule="0 */10 * * * *", arg_name="timer", run_on_startup=True)
+@app.timer_trigger(schedule="0 */15 * * * *", arg_name="timer", run_on_startup=True)
 def main(timer: func.TimerRequest) -> None:
     manager = Manager()
     new_offers, sources = manager.identify_new_offers()
