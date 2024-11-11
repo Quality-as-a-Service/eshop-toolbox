3b2193c444f37f7fef574f765cfa4493238ea04d
cherniaev egor <chrnyaevek@gmail.com>
2024-11-11 18:14:06 +0100

    Bazos

--- watchdogs/parsers/bazos.py
+++ watchdogs/parsers/bazos.py
@@ -14,5 +14,31 @@ def list_offers(query: str = "/?") -> list[str]:
     ]
 
 
+def fetch_offer_by_url(url: str):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, features="html.parser")
+
+    author = soup.select(
+        "body > div > div.flexmain > div.maincontent td.listadvlevo table tr:nth-child(1) td:nth-child(2)"
+    )
+    title = soup.select(
+        "body > div.sirka > div.flexmain > div.maincontent > div.listainzerat.inzeratyflex > div.inzeratydetnadpis > h1"
+    )
+    description = soup.select(
+        "body > div.sirka > div.flexmain > div.maincontent > div.popisdetail"
+    )
+    return {
+        "title": title[0].text if len(title) else None,
+        "description": description[0].text if len(description) else None,
+        "author": author[0].text if len(author) else None,
+    }
+
+
 if __name__ == "__main__":
-    print(list_offers())
+    # print(list_offers())
+    # print(
+    #     fetch_offer_by_url(
+    #         # "https://reality.bazos.cz/inzerat/193319261/prodej-bytu-3kk-73-m-v-plzni-slovanech.php"
+    #     )
+    # )
+    pass
