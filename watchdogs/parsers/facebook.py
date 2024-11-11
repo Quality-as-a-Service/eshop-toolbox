c519cf424b9efd5fb25fe486ecde9a51becdc178
cherniaev egor <chrnyaevek@gmail.com>
2024-11-11 17:29:07 +0100

    Facebook page fetch

--- watchdogs/parsers/facebook.py
+++ watchdogs/parsers/facebook.py
@@ -5,22 +5,24 @@ from bs4 import BeautifulSoup
 SOURCE_URL = "https://www.facebook.com/marketplace/category/propertyforsale"
 SOURCE_ITEM_URL = "https://www.facebook.com/marketplace"
 
+HEADERS = {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
+    "sec-ch-ua": '"Chromium";v="127", "Not)A;Brand";v="99"',
+    "sec-ch-ua-full-version-list": '"Chromium";v="127.0.6533.72", "Not)A;Brand";v="99.0.0.0"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-model": '""',
+    "sec-ch-ua-platform": '"Linux"',
+    "sec-ch-ua-platform-version": '"5.15.0"',
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+}
+
 
 def list_offers(query: str = "/?") -> list[str]:
     response = requests.get(
         f"{SOURCE_URL}{query}",
-        headers={
-            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-            "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
-            "sec-ch-ua": '"Chromium";v="127", "Not)A;Brand";v="99"',
-            "sec-ch-ua-full-version-list": '"Chromium";v="127.0.6533.72", "Not)A;Brand";v="99.0.0.0"',
-            "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-model": '""',
-            "sec-ch-ua-platform": '"Linux"',
-            "sec-ch-ua-platform-version": '"5.15.0"',
-            "sec-fetch-dest": "document",
-            "sec-fetch-mode": "navigate",
-        },
+        headers=HEADERS,
     )
     soup = BeautifulSoup(response.content, features="html.parser")
     data = json.loads(
@@ -39,5 +41,33 @@ def list_offers(query: str = "/?") -> list[str]:
     return [f'{SOURCE_ITEM_URL}/item/{node["node"]["listing"]["id"]}' for node in data]
 
 
+def fetch_offer_by_url(url: str):
+    response = requests.get(url, headers=HEADERS)
+    soup = BeautifulSoup(response.content, features="html.parser")
+    data = json.loads(
+        [
+            el
+            for el in soup.select('script[type="application/json"]')
+            if "GroupCommerceProductItem" in str(el)
+            and "marketplace_product_details_page" in str(el)
+        ]
+        .pop()
+        .text
+    )
+    data = data["require"][0][3][0]["__bbox"]["require"][3][3][1]["__bbox"]["result"][
+        "data"
+    ]["viewer"]["marketplace_product_details_page"]["target"]
+
+    return {
+        "author": None,
+        "title": data["base_marketplace_listing_title"],
+        "description": data["redacted_description"]["text"],
+    }
+
+
 if __name__ == "__main__":
-    print(list_offers())
+    # print(list_offers())
+    # print(
+    #     fetch_offer_by_url("https://www.facebook.com/marketplace/item/1023704802823782")
+    # )
+    pass
